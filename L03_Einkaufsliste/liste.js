d50456b7ade43414b5328409e2989dbb63dae31b
Amelie0108 <149068782+Amelie0108@users.noreply.github.com>
2024-04-19 19:20:04 +0200

    L03

---
+++ L03_Einkaufsliste/liste.js
@@ -0,0 +1,43 @@
+"use strict";
+var shoppinglist;
+(function (shoppinglist) {
+    document.addEventListener("DOMContentLoaded", () => {
+        let ProductI = document.getElementById("product");
+        let AmountI = document.getElementById("amount");
+        let DateI = document.getElementById("date");
+        let CommentI = document.getElementById("comments");
+        let CheckI = document.getElementById("check1");
+        let Check2I = document.getElementById("check2");
+        if (ProductI) {
+            ProductI.addEventListener("input", () => {
+                console.log("Product has been added");
+            });
+        }
+        if (AmountI) {
+            AmountI.addEventListener("change", () => {
+                console.log("Amount has been changed");
+            });
+        }
+        if (DateI) {
+            DateI.addEventListener("change", () => {
+                console.log("Date has been changed");
+            });
+        }
+        if (CommentI) {
+            CommentI.addEventListener("input", () => {
+                console.log("Comment has been added");
+            });
+        }
+        if (CheckI) {
+            CheckI.addEventListener("change", () => {
+                console.log("Product should be purchased");
+            });
+        }
+        if (Check2I) {
+            Check2I.addEventListener("change", () => {
+                console.log("Product has been purchased");
+            });
+        }
+    });
+})(shoppinglist || (shoppinglist = {}));
+//# sourceMappingURL=liste.js.map
