2bffc4775c9d255025233f5bb422a12b8978acbe
Amelie0108 <149068782+Amelie0108@users.noreply.github.com>
2024-04-28 00:40:47 +0200

    L03

--- L03_Einkaufsliste/liste.js
+++ L03_Einkaufsliste/liste.js
@@ -1,30 +1,55 @@
 "use strict";
 var shoppinglist;
 (function (shoppinglist) {
+    let shoppingList = [
+        { product: "Milk", amount: 2, date: "2023-04-01", comment: "For breakfast", toBuy: false, bought: true },
+        { product: "Bread", amount: 1, date: "2023-04-02", comment: "glutenfree", toBuy: true, bought: false }
+    ];
+    function addItem(product, amount, date, comment, toBuy, bought) {
+        let newItem = { product, amount, date, comment, toBuy, bought };
+        shoppingList.push(newItem);
+        console.log('Item added:', newItem);
+    }
+    function removeItem(index) {
+        shoppingList.splice(index, 1);
+        console.log('Item removed at index:', index);
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+        let addButton = document.getElementById('addButton');
+        addButton.addEventListener('click', () => {
+            let product = document.getElementById('product').value;
+            let amount = parseInt(document.getElementById('amount').value, 10);
+            let date = document.getElementById('date').value;
+            let comment = document.getElementById('comments').value;
+            let toBuy = document.getElementById('check1').checked;
+            let bought = document.getElementById('check2').checked;
+            addItem(product, amount, date, comment, toBuy, bought);
+        });
+    });
     document.addEventListener("DOMContentLoaded", () => {
-        let ProductI = document.getElementById("product");
-        let AmountI = document.getElementById("amount");
-        let DateI = document.getElementById("date");
-        let CommentI = document.getElementById("comments");
+        let product = document.getElementById("product");
+        let amount = document.getElementById("amount");
+        let date = document.getElementById("date");
+        let comment = document.getElementById("comments");
         let CheckI = document.getElementById("check1");
         let Check2I = document.getElementById("check2");
-        if (ProductI) {
-            ProductI.addEventListener("input", () => {
+        if (product) {
+            product.addEventListener("input", () => {
                 console.log("Product has been added");
             });
         }
-        if (AmountI) {
-            AmountI.addEventListener("change", () => {
+        if (amount) {
+            amount.addEventListener("change", () => {
                 console.log("Amount has been changed");
             });
         }
-        if (DateI) {
-            DateI.addEventListener("change", () => {
+        if (date) {
+            date.addEventListener("change", () => {
                 console.log("Date has been changed");
             });
         }
-        if (CommentI) {
-            CommentI.addEventListener("input", () => {
+        if (comment) {
+            comment.addEventListener("input", () => {
                 console.log("Comment has been added");
             });
         }
--- L03_Einkaufsliste/liste.ts
+++ L03_Einkaufsliste/liste.ts
@@ -1,34 +1,75 @@
 namespace shoppinglist{   
 
+    interface ShoppingItem {
+        product: string;
+        amount: number;
+        date: string;
+        comment: string;
+        toBuy: boolean;
+        bought: boolean;
+    }
+
+    let shoppingList: ShoppingItem[] = [
+        { product: "Milk", amount: 2, date: "2023-04-01", comment: "For breakfast", toBuy: false, bought: true },
+        { product: "Bread", amount: 1, date: "2023-04-02", comment: "glutenfree", toBuy: true, bought: false }
+    ];
+
+    function addItem(product: string, amount: number, date: string, comment: string, toBuy: boolean, bought: boolean): void {
+        let newItem: ShoppingItem = { product, amount, date, comment, toBuy, bought };
+        shoppingList.push(newItem);
+        console.log('Item added:', newItem);
+    }
+
+    function removeItem(index: number): void {
+        shoppingList.splice(index, 1);
+        console.log('Item removed at index:', index);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let addButton = document.getElementById('addButton') as HTMLButtonElement;
+
+    addButton.addEventListener('click', () => {
+        let product = (document.getElementById('product') as HTMLInputElement).value;
+        let amount = parseInt((document.getElementById('amount') as HTMLInputElement).value, 10);
+        let date = (document.getElementById('date') as HTMLInputElement).value;
+        let comment = (document.getElementById('comments') as HTMLTextAreaElement).value;
+        let toBuy = (document.getElementById('check1') as HTMLInputElement).checked;
+        let bought = (document.getElementById('check2') as HTMLInputElement).checked;
+
+        addItem(product, amount, date, comment, toBuy, bought);
+    });
+    });
+
     document.addEventListener("DOMContentLoaded", () => {
 
-        let ProductI = document.getElementById("product");
-        let AmountI = document.getElementById("amount");
-        let DateI = document.getElementById("date");
-        let CommentI = document.getElementById("comments");
+        let product = document.getElementById("product");
+        let amount = document.getElementById("amount");
+        let date = document.getElementById("date");
+        let comment = document.getElementById("comments");
         let CheckI = document.getElementById("check1");
         let Check2I = document.getElementById("check2");
     
-        if (ProductI) {
-            ProductI.addEventListener("input", () => {
+
+        if (product) {
+            product.addEventListener("input", () => {
                 console.log("Product has been added");
             });
         }
     
-        if (AmountI) {
-            AmountI.addEventListener("change", () => {
+        if (amount) {
+            amount.addEventListener("change", () => {
                 console.log("Amount has been changed");
             });
         }
     
-        if (DateI) {
-            DateI.addEventListener("change", () => {
+        if (date) {
+            date.addEventListener("change", () => {
                 console.log("Date has been changed");
             });
         }
     
-        if (CommentI) {
-            CommentI.addEventListener("input", () => {
+        if (comment) {
+            comment.addEventListener("input", () => {
                 console.log("Comment has been added");
             });
         }
@@ -45,5 +86,7 @@ namespace shoppinglist{
             });
         }
     });
+
     }
 
+    
